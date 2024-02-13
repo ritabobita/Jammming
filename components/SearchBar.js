8d07068495e58f068734b0d6583b7fad13e268ad
ritabobitaritabobita <rrosiek97@gmail.com>
2024-02-12 19:43:14 -0500

    create succesful transition for search bar onChange

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,40 +1,21 @@
 import styles from '../styles/SearchBar.module.css'
+import { useState } from 'react';
 
 /*NEXT STEP: create a grid that changes as input is placed inside the search bar, the search bar should expand 
              to the top of the page -- new grid should likely be attached to a new class to change on input
  */
 function SearchBar() {
-    function handleClick() {
-        const search = document.querySelector(`.${styles.searchInput}`);
-        search.classList.add(styles.searchExpanded);
+    const [isExpanded, setExpanded] = useState(false);
+
+    function handleChange() {
+        setExpanded(true)
     }
 
     return (
         <div className={styles.searchContainer}>
-            <input type='search' placeholder="Search Music.." className={styles.searchInput} onChange={handleClick} ></input>
+            <input type='search' placeholder="Search Music.." className={`${styles.searchInput} ${isExpanded ? styles.searchExpanded : ''}`} onChange={handleChange}></input>
         </div>
     )
 }
-
-/*
-const SearchBar = () => {
-    const handleExpand = () => {
-      const search = document.querySelector(`.${styles.searchInput}`);
-      search.classList.toggle(styles.searchExpanded);
-    };
-  
-    return (
-      <div className={styles.container}>
-        <button className={styles.searchWrapper} onClick={handleExpand}>
-        </button>
-  
-        <input
-          className={styles.searchInput}
-          type="search"
-          placeholder="Search keyword"
-        />
-      </div>
-    );
-  };*/
 
 export default SearchBar
