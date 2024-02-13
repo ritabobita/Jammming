1db30814788fd9b6e7b2a09a9c47abb211bd44b1
ritabobitaritabobita <rrosiek97@gmail.com>
2024-02-13 17:36:12 -0500

    create new grid change

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -3,17 +3,20 @@ import { useState } from 'react';
 
 /*NEXT STEP: create a grid that changes as input is placed inside the search bar, the search bar should expand 
              to the top of the page -- new grid should likely be attached to a new class to change on input
+             - start w/ new grid creation in home module -- may not need expanded feature for search bar maybe it could be done
+             by default with a larger search bar space at the top
+             **grid has been created, 
  */
-function SearchBar() {
-    const [isExpanded, setExpanded] = useState(false);
+function SearchBar({ onSearchInputChange }) {
 
-    function handleChange() {
-        setExpanded(true)
+    function handleChange(e) {
+        const inputValue = e.target.value;
+        onSearchInputChange(inputValue)
     }
 
     return (
         <div className={styles.searchContainer}>
-            <input type='search' placeholder="Search Music.." className={`${styles.searchInput} ${isExpanded ? styles.searchExpanded : ''}`} onChange={handleChange}></input>
+            <input type='search' placeholder="Search Music.." className={styles.searchInput} onChange={handleChange}></input>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar';
 
 
 export default function Home() {
+  const [searchInput, setSearchInput] = useState(false);
 
+  //useEffect? -- https://react.dev/reference/react/useEffect
+  useEffect(() => {
 
+  })
+
+  function handleSearchInput() {
+      setSearchInput(true);
+  }
+
+  console.log("Component re-rendered")
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Jammming</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <SearchBar />
-    </div>
+      <div className={`${searchInput ? styles.containerChange : styles.container }`}>
+          <Head>
+              <title>Jammming</title>
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <SearchBar onSearchInputChange={handleSearchInput} />
+      </div>
   );
 }
